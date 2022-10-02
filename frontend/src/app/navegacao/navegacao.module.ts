f288bc8c8f1f1d369f9b017beaa650ca2d2163ec
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-02 00:14:30 -0300

    feat: Customizando a navegacao

--- frontend/src/app/navegacao/navegacao.module.ts
+++ frontend/src/app/navegacao/navegacao.module.ts
@@ -2,13 +2,28 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { MenuComponent } from "./menu/menu.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
 @NgModule({
-    declarations: [],
+    declarations: [
+        FooterComponent,
+        HomeComponent,
+        MenuComponent,
+        NotFoundComponent,
+    ],
     imports: [
         CommonModule,
         RouterModule,
     ],
-    exports: [],
+    exports: [
+        FooterComponent,
+        HomeComponent,
+        MenuComponent,
+        NotFoundComponent,
+    ],
 })
 
 export class NavegacaoModule { }

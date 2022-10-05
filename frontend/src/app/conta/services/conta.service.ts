ebdcd418b4acdc601439f93052170f6056d88fae
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-05 10:38:25 -0300

    feat: Comunicando com api via http e base-service para consumir api

--- frontend/src/app/conta/services/conta.service.ts
+++ frontend/src/app/conta/services/conta.service.ts
@@ -1,14 +1,28 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, map, Observable } from "rxjs";
+import { BaseService } from "src/app/services/base.service";
 import { Usuario } from "../models/usuario";
 
 @Injectable()
-export class ContaService
+export class ContaService extends BaseService
 {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient)
+  {
+    super();
+  }
 
-  registrarUsuario(usuario: Usuario) { }
+  registrarUsuario(usuario: Usuario): Observable<Usuario>
+  {
+    let response = this.http.post(this.urlServiceV1 + "nova-conta", usuario, this.obterHeaderJson())
+      .pipe(
+        map(this.extractData),
+        catchError(this.serviceError),
+      );
+
+    return response;
+  }
 
   login(usuario: Usuario) { }
 }

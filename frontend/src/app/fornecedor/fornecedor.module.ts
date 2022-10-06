8fe388ef3f82712956b01d0417940896a23de641
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-05 22:22:03 -0300

    feat: Finalizando telas crud de fornecedores

--- frontend/src/app/fornecedor/fornecedor.module.ts
+++ frontend/src/app/fornecedor/fornecedor.module.ts
@@ -2,15 +2,23 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DetalhesComponent } from "./detalhes/detalhes.component";
+import { EditarComponent } from './editar/editar.component';
 import { FornecedorAppComponent } from "./fornecedor.app.component";
 import { fornecedorRoutingModule } from "./fornecedor.router";
 import { ListaComponent } from './lista/lista.component';
 import { FornecedorService } from "./services/fornecedor.service";
+import { ExcluirComponent } from './excluir/excluir.component';
+import { NovoComponent } from './novo/novo.component';
 
 @NgModule({
   declarations: [
     FornecedorAppComponent,
     ListaComponent,
+    DetalhesComponent,
+    EditarComponent,
+    ExcluirComponent,
+    NovoComponent,
   ],
   imports: [
     fornecedorRoutingModule,

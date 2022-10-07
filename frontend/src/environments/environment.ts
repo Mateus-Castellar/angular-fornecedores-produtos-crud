2fe5a54cb97b69da07e6f8b1453c74b7c5c3399a
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-07 17:35:41 -0300

    checkpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  imagensUrl: 'https://localhost:7196/'
 };
 
 /*
